36443b8ee73d81ac40a73566af6583235f763622
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-01 15:57:09 +0000

    Overlaps (╯°□°）╯︵ ┻━┻

--- 01.fsx
+++ 01.fsx
@@ -1,27 +1,27 @@
 open System.Text.RegularExpressions
 open System
 
-let readLines = System.IO.File.ReadLines("01.txt");;
+let readLines: Collections.Generic.IEnumerable<string> = System.IO.File.ReadLines("01.txt");;
 
 printfn "%A"  readLines
 
-let testLine = "eightwothree"
+let testLine: string = "eightwo"
 
 // Part one is just @"\d"
-let rx = Regex(@"\d|one|two|three|four|five|six|seven|eight|nine", RegexOptions.Compiled)
+let rx: Regex = Regex(@"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", RegexOptions.Compiled)
 
-let matchVal theMatch = 
+let matchVal (theMatch: string) = 
     match theMatch with
         | theMatch when String.length theMatch = 1 -> theMatch
-        | _ -> string (List.findIndex (fun (x) -> x = theMatch) ["one";"two";"three";"four";"five";"six";"seven";"eight";"nine"] + 1) 
+        | _ -> string (List.findIndex (fun (x: string) -> x = theMatch) ["one";"two";"three";"four";"five";"six";"seven";"eight";"nine"] + 1) 
 
-let calVal theLine = 
-    let m = rx.Matches(theLine)
+let calVal (theLine: string) = 
+    let m: MatchCollection = rx.Matches(theLine)
     //nb the inner + is to concatanate the two digit strings
-    let outSum = int ((matchVal (m.Item(0).Value)) + (matchVal (m.Item(m.Count-1).Value)))
+    let outSum: int = int ((matchVal (m.Item(0).Groups[1].Value)) + (matchVal (m.Item(m.Count-1).Groups[1].Value)))
     outSum
 
-printfn "%d" (calVal testLine)
+//printfn "%d" (calVal testLine)
 
-let resultSum = Seq.map (fun itLine -> calVal itLine) readLines |> Seq.sum
+let resultSum: int = Seq.map (fun (itLine: string) -> calVal itLine) readLines |> Seq.sum
 printfn "Tot %d" resultSum
