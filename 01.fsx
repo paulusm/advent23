e9ddfb3e92aff1af05803ff3f87c025bdb5ac431
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-01 10:11:10 +0000

    surely?

--- 01.fsx
+++ 01.fsx
@@ -5,21 +5,23 @@ let readLines = System.IO.File.ReadLines("01.txt");;
 
 printfn "%A"  readLines
 
-let testLine = "b7fgghh"
+let testLine = "eightwothree"
 
+// Part one is just @"\d"
 let rx = Regex(@"\d|one|two|three|four|five|six|seven|eight|nine", RegexOptions.Compiled)
 
 let matchVal theMatch = 
     match theMatch with
         | theMatch when String.length theMatch = 1 -> theMatch
-        | _ -> (List.findIndex (fun (x,_) -> x = theMatch) ["one";"two";"three";"four";"five";"six";"seven";"eight";"nine"]) + 1
+        | _ -> string (List.findIndex (fun (x) -> x = theMatch) ["one";"two";"three";"four";"five";"six";"seven";"eight";"nine"] + 1) 
 
 let calVal theLine = 
     let m = rx.Matches(theLine)
+    //nb the inner + is to concatanate the two digit strings
     let outSum = int ((matchVal (m.Item(0).Value)) + (matchVal (m.Item(m.Count-1).Value)))
     outSum
 
-//printfn "%d" (calVal testLine)
+printfn "%d" (calVal testLine)
 
-let partTwo = Seq.map (fun itLine -> calVal itLine) readLines |> Seq.sum
-printfn "Tot %d" partOne
+let resultSum = Seq.map (fun itLine -> calVal itLine) readLines |> Seq.sum
+printfn "Tot %d" resultSum
