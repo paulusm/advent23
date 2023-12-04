c13751a122d0abb9e3fd705df3f81b34d6f8ed38
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-04 07:45:22 +0000

    4

---
+++ 04.fsx
@@ -0,0 +1,17 @@
+open System.Text.RegularExpressions
+
+let inputLines = System.IO.File.ReadLines("04.txt")
+
+let rxPart: Regex = Regex(@"\d+", RegexOptions.Compiled)
+
+let allParts = inputLines |> Seq.map(fun dataLine -> 
+  let cardParts = dataLine.Substring(9).Split("|")
+  let winnumMatches:MatchCollection = cardParts.[0] |> rxPart.Matches
+  let mynumMatches:MatchCollection = cardParts.[1] |> rxPart.Matches
+  let wins:seq<string> = winnumMatches |> Seq.map(fun x -> x.Value) |> Seq.filter(fun x -> mynumMatches |> Seq.exists(fun y->y.Value=x) )
+  let wincount:seq<int>= wins |> Seq.map(fun x-> int x) 
+  let winnum:int = wincount |> Seq.length
+  winnum
+) 
+let tot = allParts |> Seq.filter(fun x-> x>0) |> Seq.map(fun x -> 2.0 ** float(x-1) ) |> Seq.sum
+printfn "Part One - %A" tot
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@
 |  [1](01.fsx)  |   OK, Second part took ages     |
 |  [2](02.fsx)  |   Very horrible code for Part 2  esp.    |
 |  [3](03.fsx)  |   Spent a HUGE amount of time on this   |
+|  [4](04.fsx)  |   Run out of time to do part 2, maybe later   |
