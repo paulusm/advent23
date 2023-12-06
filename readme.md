4f3cc5d3fe513b1ba260c41ab05b81124453762f
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-06 10:17:40 +0000

    tried again

--- readme.md
+++ readme.md
@@ -8,4 +8,4 @@
 |  [2](scripts/02.fsx)  |   Very horrible code for Part 2  esp.    |
 |  [3](scripts/03.fsx)  |   Spent a HUGE amount of time on this   |
 |  [4](scripts/04.fsx)  |   Run out of time to do part 2, maybe later   |
-|  [5](scripts/05.fsx)  |   Some *bads* code. Part 1 OK, part 2 CPU nearly combusted - no result  |
+|  [5](scripts/05.fsx)  |   Some *bad* code. Part 1 OK, part 2 memory leak or something 🔥  |
--- scripts/05.fsx
+++ scripts/05.fsx
@@ -5,7 +5,7 @@ open System.Linq.Expressions
 
 let input = System.IO.File.ReadAllText("../data/05.txt") 
 
-// key: the destination range start, the source range start, and the range length.
+// Key: the destination range start, the source range start, and the range length.
 
 let rxData: Regex = Regex(@"\d+", RegexOptions.Compiled)
 
@@ -13,14 +13,14 @@ let sections = (Regex.Split(input,@":" ) |>
      Array.map(fun x-> x |> rxData.Matches) |> 
      Array.filter(fun x -> x |> Seq.length > 0 ))
 
-//Let's convert to big int for convenience
+// Let's convert to big int for convenience
 let lookups = sections |> Array.map(fun x -> x |> Seq.map (fun y-> int64(y.Value)))
 
 let rec lookup x theSeq = 
      let result=(theSeq |> Seq.chunkBySize 3  |> 
                Seq.map(fun (z:array<int64>)->
                //Triple of dest, source and range length z
-                (if x < z.[1] + z.[2] && x >= z.[1] then x - z.[1] + z.[0] else 0)
+                (if x < z.[1] + z.[2] && x >= z.[1] then x - z.[1] + z.[0] else 0L)
                ) |> Seq.sum )
      match result with
           | 0L -> x
@@ -44,7 +44,7 @@ printfn "%A" (result |> Seq.min)
 // Part Two
 
 let seeds2 = (lookups.[0] |> Seq.chunkBySize(2) |> 
-     Seq.map(fun x ->[x.[0]..(x.[0] + x[1]-1L)]) |> 
+     Seq.map(fun x ->[x.[0]..(x.[0] + x.[1] - 1L)]) |> 
      Seq.toList |> List.concat)
 
 // TODO refactor this abomination :angry:😮
