8c93ab83da5c60264c874794c7d44e4630da2bb2
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-10 08:04:36 +0000

    start 09

--- scripts/09.fsx
+++ scripts/09.fsx
@@ -5,11 +5,22 @@ let input = System.IO.File.ReadLines("../data/09-test.txt")
 
 let rxSensorData: Regex = Regex(@"\d+", RegexOptions.Compiled)
 
-let sensorRows = input |> Seq.map (fun x -> rxSensorData.Matches x) 
+let sensorRows:seq<seq<seq<int>>> = (input |> Seq.map (fun x -> (rxSensorData.Matches x)) |> 
+    Seq.map(fun x-> x |> Seq.map(fun y ->int y.Value)) |> 
+    Seq.map(fun x-> seq{x}))
 
-let computeDiff = sensorRows |> Seq.map(fun x ->
-    x |> Seq.pairwise |> Seq.map(fun a -> int (snd a).Value - int (fst a).Value)
-)
+let rec computeDiff (theRow:seq<seq<int>>):seq<seq<int>> = 
+    let resultRow = theRow |> Seq.last |> Seq.pairwise |> Seq.map(fun s -> (snd s) - (fst s))
+    let zeroCheck = resultRow |> Seq.sum 
+    let resultSeq = seq {resultRow}
+    match zeroCheck with
+     | 0 -> resultSeq
+     | _ -> Seq.append theRow (computeDiff resultSeq)
 
+let processSeqs:seq<seq<seq<int>>> =
+    sensorRows |> Seq.map(fun x -> //x is our sequence of transformations
+        let theNextRow:seq<seq<int>> = computeDiff (x)
+        theNextRow
+    )
 
-printfn "%A" (Seq.item 0 computeDiff |> Seq.iter (printfn "%A"))
+printfn "%A" (processSeqs |> Seq.item 0 |> Seq.item 1)
