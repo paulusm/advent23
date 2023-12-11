0a106a5c63e59fac99eb16a1bc53de8355f65f22
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-11 17:47:17 +0000

    er, how?

--- scripts/11.fsx
+++ scripts/11.fsx
@@ -1,16 +1,20 @@
-open System.Text.RegularExpressions
-open System
 
-let theImageRaw = System.IO.File.ReadLines("../data/11-test.txt") |> Seq.toArray |> Array.map(fun x-> Seq.toArray x) 
+
+let theImageRaw = System.IO.File.ReadLines("./data/11-test.txt") |> Seq.toArray |> Array.map(fun x-> Seq.toArray x) 
 let theImage = array2D theImageRaw
 
 let theImageDim = Array.length theImage.[0,*]-1
 
-let rowBlank = [0..theImageDim] |> List.map(fun x ->
-    theImage.[x,*] |> Array.exists(fun x ->
-    x='#'
+let rowHasGalaxy = [|0..theImageDim|] |> Array.map(fun x -> theImage.[x,*] |> Array.exists(fun x -> x = '#'))
+let colHasGalaxy = [|0..theImageDim|] |>  Array.map(fun x -> theImage.[*,x] |> Array.exists(fun x -> x = '#'))
+
+
+let expandBlanks inImage = (rowHasGalaxy |> Array.mapi(fun i x->
+    match x with
+        | true -> inImage //|> Array.insertManyAt i inImage [|'t';'t';'t';|]
+        | false -> inImage
     )
 )
 
 
-printfn "%A" rowBlank
+printfn "%A" (expandBlanks theImage)
