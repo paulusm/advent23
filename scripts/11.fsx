10745e104e0fcfb837cd12871fa1dabda0770454
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-11 07:37:09 +0000

    OK

--- scripts/11.fsx
+++ scripts/11.fsx
@@ -1,8 +1,16 @@
 open System.Text.RegularExpressions
 open System
 
-let my2DArray = array2D [ [ 1; 0]; [0; 1] ]
 let theImageRaw = System.IO.File.ReadLines("../data/11-test.txt") |> Seq.toArray |> Array.map(fun x-> Seq.toArray x) 
 let theImage = array2D theImageRaw
 
-printfn "%A" (theImage.[3,3])
+let theImageDim = Array.length theImage.[0,*]-1
+
+let rowBlank = [0..theImageDim] |> List.map(fun x ->
+    theImage.[x,*] |> Array.exists(fun x ->
+    x='#'
+    )
+)
+
+
+printfn "%A" rowBlank
