bc4cd7f4d9fc57a4f2e23e04870f87ef3ae5d713
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-11 20:54:21 +0000

    pause 11

--- scripts/11.fsx
+++ scripts/11.fsx
@@ -1,6 +1,6 @@
 
 
-let theImageRaw = System.IO.File.ReadLines("./data/11-test.txt") |> Seq.toArray |> Array.map(fun x-> Seq.toArray x) 
+let theImageRaw = System.IO.File.ReadLines("../data/11-test.txt") |> Seq.toArray |> Array.map(fun x-> Seq.toArray x) 
 let theImage = array2D theImageRaw
 
 let theImageDim = Array.length theImage.[0,*]-1
@@ -9,12 +9,19 @@ let rowHasGalaxy = [|0..theImageDim|] |> Array.map(fun x -> theImage.[x,*] |> Ar
 let colHasGalaxy = [|0..theImageDim|] |>  Array.map(fun x -> theImage.[*,x] |> Array.exists(fun x -> x = '#'))
 
 
-let expandBlanks inImage = (rowHasGalaxy |> Array.mapi(fun i x->
-    match x with
-        | true -> inImage //|> Array.insertManyAt i inImage [|'t';'t';'t';|]
-        | false -> inImage
+let expandBlanks how (inImage:char[,])= (
+    [|0..theImageDim|] |> Array.mapi(fun i x->
+        let checkBlanks = if how="row" then rowHasGalaxy else colHasGalaxy
+        match checkBlanks.[i] with
+        | false when how="row" -> [|inImage.[i,*];inImage.[i,*]|]
+        | true when how="row" -> [|inImage.[i,*]|]
+        | false when how="col" -> [|inImage.[*,i];inImage.[*,i]|]
+        | true when how="col" -> [|inImage.[*,i]|]
+        | _ -> [|inImage.[i,*]|]
     )
 )
 
-
-printfn "%A" (expandBlanks theImage)
+let expandedRows = expandBlanks "row" theImage |> Array.map (fun x->
+     x |> Array.map(fun y->y)
+)
+printfn "%A" (expandedRows)
