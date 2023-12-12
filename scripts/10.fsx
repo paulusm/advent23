7163a6ce16aec8412c6eb5d47dec400c39383db0
Paul Matthews <paulusm@gmail.com>
2023-12-12 22:20:26 +0000

    oh

--- scripts/10.fsx
+++ scripts/10.fsx
@@ -7,20 +7,21 @@ let moves = [[1;0];[-1;0];[0;1];[0;-1]]
 
 let startPos = [0;1]
 
-let possMoves (theStartPos:list<int>) = 
+let possMoves (theStartPos:list<int>) (thePathHistory:list<list<int>>) = 
     moves |> List.map(fun x->
         let possPos = [theStartPos.[0] +  x.[0]; theStartPos.[1] +  x.[1]]
         possPos
-    ) |> List.filter(fun x-> x.[0] > -1 && x.[1] > -1)
+    ) |> List.filter(fun x-> x.[0] > -1 && x.[1] > -1 && not(List.contains theStartPos thePathHistory))
 
-let rec moveCount (thePos:list<int>) (theAggregateStepCount:list<int>):list<list<int>>   =
-    let moveOptions = possMoves thePos
+let rec moveCount (thePos:list<int>) (pathHist:list<list<int>>):list<list<int>>   =
+    let pathHistRet = List.insertAt 0 thePos pathHist
+    let moveOptions = possMoves thePos pathHist
     moveOptions |> List.map(fun x ->
         let moveChar = theMap.[x.[0],x.[1]]
         printfn $"Move %s{(moveChar).ToString()}"
         match moveChar with
-        | '.' -> List.append theAggregateStepCount [0]
-        | _ ->   List.append theAggregateStepCount [1] //(moveCount x [(theAggregateStepCount + 1)]]
+        | '.' -> pathHistRet
+        | _ ->   List.append pathHistRet [1] //(moveCount x [(theAggregateStepCount + 1)]]
     ) 
 
-printfn "%A" (moveCount startPos [1])
+printfn "%A" (moveCount startPos [])
