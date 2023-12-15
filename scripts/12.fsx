749c83b7580cf1a984b44a8e3c641e69f13090e6
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-15 17:26:52 +0000

    Get out of my head!

--- scripts/12.fsx
+++ scripts/12.fsx
@@ -1,9 +1,30 @@
+ 
+open System.Text.RegularExpressions
+let inputData = System.IO.File.ReadLines("../data/12-test.txt") 
 
-let inputData = System.IO.File.ReadLines("./data/12-test.txt") 
+let rec runMatches  (theString:string) (theNo:string) (theLastPos:int) = 
+    let theTestString = theString[theLastPos..]
+    printfn "%d" theLastPos
+    let reTest =  Regex ("([#?]{" + theNo + "})(^#|$)")
+    let hashMatch = reTest.Match theTestString
+    printfn "%A" hashMatch.Groups.[1]
+    if not hashMatch.Success then
+        let reTest2 =  Regex ("([#?]{" + theNo + "})[.$]{1}")
+        let varMatch = reTest2.Match theTestString 
+        printfn "%A" varMatch.Groups.[1]
+        (varMatch.Groups.[1].Index + varMatch.Groups.[1].Value.Length-1, varMatch.Groups.[1].Value)
+    else
+        (hashMatch.Groups.[1].Index + hashMatch.Groups.[1].Value.Length-1, hashMatch.Groups.[1].Value)
 
 let result = inputData |> Seq.map(fun x ->
     let springLine = x.Split ' '
-    springLine
+    let mutable startPos = 0
+    springLine.[1].Split ',' |> Array.map(fun seqLen ->
+        // Call for each segment
+        let (newStart, theMatch) = runMatches springLine.[0] seqLen (startPos)
+        startPos <- newStart
+        theMatch
+    )
 )
 
-printfn "%A" result
+printfn "%A" (result |> Seq.map(fun x-> x))
