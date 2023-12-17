7c8bbb05a198feb1b005c029b59ed160883f1edb
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-17 09:08:14 +0000

    15 pt 2 is killer

--- scripts/15.fsx
+++ scripts/15.fsx
@@ -26,23 +26,47 @@ let boxValue theStep =
     ) |> Seq.reduce(fun acc charVal-> ((acc + charVal) * 17) % 256)
 
 let rxInstruction = Regex(@"([a-z]+)([=-])(\d*)", RegexOptions.Compiled)
-let boxMap = Map.empty
+//let boxMap = Map.empty
 
-let lensOp theBox theOp theLens theVal :Map<string,string> = 
-    let lensMap = if boxMap.ContainsKey theBox then boxMap[theBox] else Map.empty
+let doRemove (lensMap:Map<int,string* string>) (theLens:string) = 
+    let mapKeys = lensMap.Keys
+    let theKey = (mapKeys |> Seq.map(fun x->
+        let (lensName,lensVal) = lensMap[x]
+        if lensName = theLens then x else 0) |> Seq.max)
+    lensMap.Remove theKey 
+
+let doAdd (lensMap:Map<int,string* string>) (theNewKey:int) (theNewVal:string * string)=
+    let mapKeys = lensMap.Keys
+    let (newLens, newVal) = theNewVal
+    let theKey = 
+        if mapKeys.Count = 0 then
+             0
+        else
+            mapKeys |> Seq.map(fun x->
+                let (lensName,lensVal) = lensMap[x]
+                if lensName = newLens then x else 0) |> Seq.max
+    let insKey = if theKey=0 then theNewKey else theKey
+    lensMap.Add (insKey,theNewVal)
+   
+
+let lensOp (theBoxMap:Map<int,Map<int,string * string>>) theBox theOp (theLens:string) (theVal:string) :Map<int,string * string> = 
+    let lensMap = if theBoxMap.ContainsKey theBox then theBoxMap[theBox] else Map.empty
+    //printfn "Box %d already has %d lenses" theBox lensMap.Count
+    let nextKey = if lensMap.Count>0 then lensMap.Keys |> Seq.max else 0
     match theOp with
-    | "-" -> lensMap.Remove theLens
-    |  _  -> lensMap.Add(theLens,theVal)
+    | "-" -> doRemove lensMap theLens
+    |  _  -> doAdd lensMap (nextKey+1) (theLens,theVal)
 
-let part2Result:Map<int,Map<string,string>>[] = 
-    steps |> Array.map(fun step ->
+let boxMap:Map<int,Map<int,string * string>> = 
+    (Map.empty, steps) ||> Array.fold(fun acc step ->
     let matchStep = rxInstruction.Match step
     let boxNum = boxValue matchStep.Groups.[1].Value
-    let boxContents:Map<string, string> = lensOp boxNum matchStep.Groups.[2].Value matchStep.Groups.[1].Value matchStep.Groups.[3].Value
-    if boxMap.ContainsKey boxNum then 
-        Map.remove boxNum boxMap |> Map.add boxNum boxContents 
+    let boxContents:Map<int, string * string> = lensOp acc boxNum matchStep.Groups.[2].Value matchStep.Groups.[1].Value matchStep.Groups.[3].Value
+    //printfn "%A" boxContents
+    if acc.ContainsKey boxNum then 
+        Map.remove boxNum acc |> Map.add boxNum boxContents 
     else 
-        Map.add boxNum boxContents boxMap)
+        Map.add boxNum boxContents acc)
     
 
-printfn "Part 2 - %A" (part2Result)
+printfn "Part 2 - %A" (boxMap)
